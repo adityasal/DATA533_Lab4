a8e49d008d980649d74c21524a1767040d310735
Aditya Saluja <adisal97@student.ubc.ca>
2020-12-08 00:12:33 -0800

    added get methods in tree class to test for tree traversal

--- graphsTrees/trees/tree.py
+++ graphsTrees/trees/tree.py
@@ -1,6 +1,11 @@
 from graphsTrees.trees.node import Node
 
 class Tree:
+    
+    def __init__(self):
+        self._preorder_list = []
+        self._inorder_list = []
+        self._postorder_list = []
    
     def create_node(self,key):
         return Node(key)
@@ -20,20 +25,30 @@ class Tree:
         else:
             return self.search_node(node.left,key)
     
+    def preorder(self, node):
+        if node is not None:
+            self._preorder_list.append(node.key)
+            self.preorder(node.left)
+            self.preorder(node.right)
+    
     def inorder(self,node):
         if node is not None:
             self.inorder(node.left)
-            print(node.key)
+            self._inorder_list.append(node.key)
             self.inorder(node.right)
-    
-    def preorder(self, node):
-        if node is not None:
-            print(node.key)
-            self.preorder(node.left)
-            self.preorder(node.right)
     
     def postorder(self, node):
         if node is not None: 
             self.postorder(node.left)
             self.postorder(node.right)
-            print(node.key)
+            self._postorder_list.append(node.key)
+    
+    def get_preorder(self):
+        return self._preorder_list
+    
+    def get_inorder(self):
+        return self._inorder_list
+    
+    def get_postorder(self):
+        return self._postorder_list
+    

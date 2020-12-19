2bd348c4dfb316095b01d02b99f32d68b6bc65c4
EBAX1 <ebaxter1@hotmail.ca>
2020-12-18 17:36:33 -0800

    Added pypi files

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+name='graphsTrees',
+version='0.1',
+packages=find_packages(exclude=['tests*']),
+license='MIT',
+description='An assignment package for graph and tree data structures’,
+url='https://github.com/adityasal/DATA533_Lab4',
+author='Eric Baxter, Aditya Saluja',
+author_email='ebaxter1@hotmail.ca'
+)
